ae385dd992f7e034a282ff60e19d5f8e5d9fc90a
uu <qqwe85413@gmail.com>
2026-09-17 01:08:50 +0300

    Добавить выполнение ЛР1

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -1,1 +1,42 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using InteractiveMap.Console.Models;
+using InteractiveMap.Console.Services;
+
+MapService mapService = new MapService();
+
+PointOfInterest museum = mapService.AddPoi("МузеЙ", new Coordinate(89.001, 32.9191));
+
+void TestAddPoiLatitude1()
+{
+    PointOfInterest museum = mapService.AddPoi("museum", new Coordinate(-91, 37.6150));
+}
+
+void TestAddPoiLatitude2()
+{
+    PointOfInterest museum = mapService.AddPoi("museum", new Coordinate(91, 37.6150));
+}
+
+void TestAddPoiCoordinateNull()
+{
+    PointOfInterest museum = mapService.AddPoi("museum", null);
+}
+
+void TestFindByNameNull()
+{
+    PointOfInterest? result = mapService.FindByName(null);
+}
+
+void TestFindByNameEmpty()
+{
+    PointOfInterest? result = mapService.FindByName("");
+}
+
+void TestFindByNameIgnoreCase()
+{
+    PointOfInterest? result = mapService.FindByName("Музей");
+}
+void TestFindByNameWhiteSpace()
+{
+    PointOfInterest? result = mapService.FindByName("музей            ");
+}
+
+TestFindByNameEmpty();
