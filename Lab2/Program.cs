8217da1c1f6b05ff53cda0f207098d2c57b06d18
uu <qqwe85413@gmail.com>
2026-09-17 02:22:24 +0300

    Добавить выполнение ЛР2

---
+++ Lab2/Program.cs
@@ -0,0 +1,47 @@
+﻿using InteractiveMap.Console.Models;
+using InteractiveMap.Console.Services;
+
+MapService mapService = new MapService();
+
+// 1. Добавление точек
+PointOfInterest park = mapService.AddPoi("Центральный парк", new Coordinate(55.7601, 37.6184));
+PointOfInterest cafe = mapService.AddPoi("Кофейня на углу", new Coordinate(55.7612, 37.6205));
+PointOfInterest museum = mapService.AddPoi("Музей истории", new Coordinate(55.7580, 37.6150));
+
+Console.WriteLine("Все точки");
+
+foreach (PointOfInterest p in mapService.All)
+{
+    Console.WriteLine(p);
+}
+
+// 2. Поиск по имени
+Console.WriteLine("\nПоиск: \"Музей истории\" ");
+Console.WriteLine(mapService.FindByName("Музей истории")?.ToString());
+
+// 3. Расстояние
+Console.WriteLine($"\nРасстояние парк -> музей: {mapService.GetDistance(park.Id, museum.Id):3} км");
+
+// 4. Удаление
+Console.WriteLine($"\nУдаление #{cafe.Id}: {mapService.RemovePoi(cafe.Id)}");
+Console.WriteLine("Осталось точек: " + mapService.All.Count);
+
+mapService.AddPoi("Спортивный зал", new Coordinate(15.7580, -19.6150));
+foreach (PointOfInterest p in mapService.All)
+{
+    Console.WriteLine(p);
+}
+
+
+// 5. Проверка валидации (ожидаем ошибку)
+Console.WriteLine("\nПроверка валидации (широта 100) ===");
+
+try
+{
+    mapService.AddPoi("Ошибка", new Coordinate(100, 0));
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine($"Ожидаемая ошибка: {ex.Message}");
+}
