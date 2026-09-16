a2bcbdda2fa1974ffbe6caab21c25a6423073fff
uu <qqwe85413@gmail.com>
2026-09-17 01:05:14 +0300

    Добавить модели

---
+++ InteractiveMap.Console/Models/Coordinate.cs
@@ -0,0 +1,28 @@
+﻿namespace InteractiveMap.Console.Models;
+
+public sealed class Coordinate
+{
+    public double Latitude { get; }
+    public double Longitude { get; }
+
+    public Coordinate(double latitude, double longitude)
+    {
+        if (latitude < -90 || latitude > 90) 
+        {
+            throw new ArgumentOutOfRangeException(nameof(latitude), "Широта должна быть в диапазоне [-90; 90].");
+        }
+
+        if (longitude < -180 || longitude > 180)
+        {
+            throw new ArgumentOutOfRangeException(nameof(longitude), "Долгота должна быть в диапазоне [-180; 180].");
+        }
+
+        Latitude = latitude;
+        Longitude = longitude;
+    }
+
+    public override string ToString()
+    {
+        return $"Ширина: {Latitude} | Высота: {Longitude}";
+    }
+}
---
+++ InteractiveMap.Console/Models/PointOfInterest.cs
@@ -0,0 +1,11 @@
+﻿
+namespace InteractiveMap.Console.Models;
+
+public sealed class PointOfInterest
+{
+    public int Id { get; init; }
+    public string Name { get; set; } = string.Empty;
+    public Coordinate Coordinate { get; set; } = null!;
+
+    public override string ToString() => $"#{Id} | {Name} | {Coordinate}";
+}
