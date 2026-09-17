5b61886f86367dd3e4b0e14f32c3dc214bfc744a
uu <qqwe85413@gmail.com>
2026-09-17 04:02:50 +0300

    Добавить выполнение ЛР3

---
+++ Lab3/Program.cs
@@ -0,0 +1,20 @@
+﻿using InteractiveMap.Console.Models;
+using InteractiveMap.Console.Services;
+
+Console.WriteLine("Lab3");
+
+var map = new MapService();
+
+PointOfInterest[] points = new PointOfInterest[10];
+
+var rnd = new Random();
+
+for (int i = 0; i < points.Length; i++)
+{
+    points[i] = map.AddPoi($"Name: {i + 1} | ", new Coordinate(rnd.Next(-90, 90), rnd.Next(-180, 180)));
+}
+
+for (int i = 0; i < points.Length; i++)
+{
+    Console.WriteLine(points[i]);
+}
