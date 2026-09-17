6f83bedd4075d597bac0acae12a68de025834f4b
uu <qqwe85413@gmail.com>
2026-09-17 23:21:18 +0300

    Реализовать потокобезопасный сервис

---
+++ InteractiveMap.Console/Services/MapServiceThreadSafe.cs
@@ -0,0 +1,111 @@
+﻿using InteractiveMap.Console.Models;
+
+namespace InteractiveMap.Console.Services;
+
+public class MapServiceThreadSafe
+{
+    private readonly List<PointOfInterest> _pois = new();
+    private int _nextId = 1;
+    private readonly object _lock = new object();
+
+    public IReadOnlyList<PointOfInterest> All
+    {
+        get
+        {
+            lock (_lock)
+            {
+                return _pois;
+            }
+        }
+    }
+
+    public PointOfInterest AddPoi(string name, Coordinate coordinate)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Имя точки не может быть пустым", nameof(name));
+
+        if (coordinate is null)
+            throw new InvalidOperationException($"Параметр {nameof(coordinate)} имеет значение null");
+
+        lock (_lock)
+        {
+            PointOfInterest poi = new PointOfInterest
+            {
+                Id = _nextId++,
+                Name = name.Trim(),
+                Coordinate = coordinate
+            };
+
+            _pois.Add(poi);
+
+            return poi;
+        }
+    }
+
+    public bool RemovePoi(int id)
+    {
+        lock (_lock)
+        {
+            return _pois.RemoveAll(p => p.Id == id) > 0;
+        }
+    }
+
+    //public bool RemovePoi(int id) => _pois.Where(p => p.Id != id).Count() > 0;
+
+    public PointOfInterest? FindByName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name)) return null;
+
+        lock (_lock)
+        {
+
+            PointOfInterest? result = _pois.FirstOrDefault(p =>
+                p.Name.Equals(name.Trim(), StringComparison.OrdinalIgnoreCase));
+
+            System.Console.WriteLine($"Результат поиска: {result}");
+
+            return result;
+        }
+    }
+
+    public double GetDistance(int id1, int id2)
+    {
+        PointOfInterest a = _pois.FirstOrDefault(p => p.Id == id1)
+            ?? throw new ArgumentException($"Точка с Id={id1} не найдена.", nameof(id1));
+
+        PointOfInterest b = _pois.FirstOrDefault(p => p.Id == id2)
+            ?? throw new ArgumentException($"Точка с Id={id2} не найдена.", nameof(id2));
+
+        // Радиус земли в км.
+        const double R = 6371.0;
+
+        // Дельта между точками по широте.
+        double dLat = ToRadians(b.Coordinate.Latitude - a.Coordinate.Latitude);
+
+        // Дельта между точками по долготе.
+        double dLon = ToRadians(b.Coordinate.Longitude - a.Coordinate.Longitude);
+
+        // Перевод широт в радианы.
+        double lat1 = ToRadians(a.Coordinate.Latitude);
+        double lat2 = ToRadians(b.Coordinate.Latitude);
+
+        // Вычисление кратчайшего расстояния между точек (используется формула гаверсинуса).
+        double dlatSin = Math.Sin(dLat / 2) * Math.Sin(dLat / 2);
+        double latCos = Math.Cos(lat1) * Math.Cos(lat2);
+        double dLonSin = Math.Sin(dLon / 2) * Math.Sin(dLon / 2);
+
+        double h = dlatSin + (latCos * dLonSin);
+
+        // Вычисляем расстояние в киллометры.
+        double result = 2 * R * Math.Asin(Math.Sqrt(h));
+
+        return result;
+    }
+
+    /// <summary>
+    /// Метод переводит градусы в радианы, для работы с синусом.
+    /// </summary>
+    /// <param name="deg">Градусы.</param>
+    /// <returns>Приведенные градусы.</returns>
+    private double ToRadians(double deg) => deg * Math.PI / 180.0;
+}
