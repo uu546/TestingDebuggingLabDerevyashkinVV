08474b640fffa48836ad9634f0e968d63b1b1a68
uu <qqwe85413@gmail.com>
2026-09-18 02:25:50 +0300

    Добавить выполнение ЛР5

---
+++ Lab5/Program.cs
@@ -0,0 +1,1 @@
+﻿Console.WriteLine("Lab 5");
---
+++ TestingDebugging.Tests/CoordinateTest.cs
@@ -0,0 +1,38 @@
+﻿using InteractiveMap.Console.Models;
+
+namespace TestingDebugging.Tests;
+
+public class CoordinateTest
+{
+    [Theory]
+    [InlineData(-90, -180)]
+    [InlineData(90, 180)]
+    [InlineData(0, 0)]
+    [InlineData(55.75, 37.61)]
+    public void Constructor_ValidValues_CreatedCoordinate(double lat, double lon)
+    {
+        var coord = new Coordinate(lat, lon);
+
+        Assert.NotNull(coord);
+    }
+
+    [Theory]
+    [InlineData(-91, 0)]
+    [InlineData(91, 0)]
+    public void Constructor_InvalidLatitude_ThrowsArgumentOutOfRange(double lat, double lon)
+    {
+        Action act = () => new Coordinate(lat, lon);
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => act());
+    }
+
+    [Theory]
+    [InlineData(0, -181)]
+    [InlineData(0, 181)]
+    public void Constructor_InvalidLongitude_ThrowsArgumentOutOfRange(double lat, double lon)
+    {
+        Action act = () => new Coordinate(lat, lon);
+
+        Assert.Throws<ArgumentOutOfRangeException>(() => act());
+    }
+}
